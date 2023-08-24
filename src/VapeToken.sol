b9ee14fe082e470dbd7fedfc6b8c10a513ef55a0
Rahul Sethuram <rahul.eth@icloud.com>
2023-08-24 20:57:21 +0400

    feat: moar game stuff

--- src/VapeToken.sol
+++ src/VapeToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 import "openzeppelin/token/ERC20/ERC20.sol";
 
@@ -7,17 +7,20 @@ contract VapeToken is ERC20 {
     mapping(address => uint256) paidDividends;
 
     address payable owner;
+    address payable public rewardsContract;
 
-    uint256 devFund = 200 ether; //dev fund value = 200vape
-    uint256 potValueETH = 0;
-    uint256 totalDividendsValueETH = 0;
+    uint256 immutable public MIN_INVEST_TICK = 0.00067 ether;
 
-    uint256 ethFee = 0; //accumulated eth fee
-    uint256 minInvest = 0.0067 ether; // 0.0067 ETH
-    uint256 vapeTokenPrice = 0.0067 ether; // 0.0067 ETH
+    uint256 public devFund = 200 ether; //dev fund value = 200vape
+    uint256 public potValueETH = 0;
+    uint256 public totalDividendsValueETH = 0;
 
-    uint256 lastPurchasedTime;
-    address payable lastPurchasedAddress;
+    uint256 public collectedFee = 0; //accumulated eth fee
+    uint256 public minInvest = 0.00067 ether;
+    uint256 public vapeTokenPrice = 0.00067 ether;
+
+    uint256 public lastPurchasedTime;
+    address payable public lastPurchasedAddress;
 
     bool public isPaused = false;
 
@@ -31,19 +34,32 @@ contract VapeToken is ERC20 {
         _;
     }
 
-    constructor() ERC20("Vape", "VAPE") {
+    constructor(address _rewardsContract) ERC20("Vape", "VAPE") {
         owner = payable(msg.sender);
         lastPurchasedTime = block.timestamp;
         _mint(owner, devFund);
+        rewardsContract = payable(_rewardsContract);
     }
 
-    function addToPot() public payable notPaused {
+    function pause() public onlyOwner {
+        isPaused = true;
+    }
+
+    function unpause() public onlyOwner {
+        isPaused = false;
+    }
+
+    function takeAVapeHit() public payable notPaused {
         require(msg.value >= minInvest, "ETH value below min invest");
         require((block.timestamp - lastPurchasedTime) <= 86400, "Time is over, pot can be claimed by winner.");
 
         uint256 amount = (msg.value * 85000) / 100000; // 10% for stakers and 5% for fees = 85% for pot + dividend
-        uint256 fee = msg.value - amount;
-        ethFee += fee;
+        uint256 rewards = (msg.value * 10000) / 100000; // 10% for stakers
+        uint256 fee = msg.value - (amount + rewards);
+
+        // rewardsContract.transfer(rewards); // 10% for stakers transferred to rewards contract
+
+        collectedFee += fee;
         potValueETH += amount / 2;
         totalDividendsValueETH += amount / 2;
 
@@ -52,8 +68,8 @@ contract VapeToken is ERC20 {
         lastPurchasedTime = block.timestamp;
         lastPurchasedAddress = payable(msg.sender);
 
-        minInvest = minInvest + 0.0134 ether; // 0.00134 ETH
-        vapeTokenPrice = vapeTokenPrice + 0.0067 ether; // 0.0067 ETH
+        minInvest = minInvest + (MIN_INVEST_TICK * 2);
+        vapeTokenPrice = vapeTokenPrice + MIN_INVEST_TICK;
 
         _mint(msg.sender, vapetokenvalue);
     }
@@ -65,7 +81,7 @@ contract VapeToken is ERC20 {
         return share;
     }
 
-    function payMyDividend() public notPaused {
+    function payMyDividend() public {
         require(getMyDividend(msg.sender) > 0, "No dividend for payout");
         uint256 remainingDividend = getMyDividend(msg.sender);
         paidDividends[msg.sender] += remainingDividend;
@@ -73,11 +89,11 @@ contract VapeToken is ERC20 {
     }
 
     function paydDevFee() public onlyOwner {
-        owner.transfer(ethFee);
-        ethFee = 0;
+        owner.transfer(collectedFee);
+        collectedFee = 0;
     }
 
-    function payPotToWinner() public notPaused {
+    function takeTheLastHit() public notPaused {
         require((block.timestamp >= lastPurchasedTime), "No."); //86400
         require((block.timestamp - lastPurchasedTime) > 86400, "Time is not over yet, countdown still running.");
         lastPurchasedAddress.transfer(getETHPotValue());
@@ -98,7 +114,7 @@ contract VapeToken is ERC20 {
     }
 
     function getEthFeesValue() public view returns (uint256) {
-        return ethFee;
+        return collectedFee;
     }
 
     function getLastPurchasedTime() public view returns (uint256) {
---
+++ test/VapeToken.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+
+import {VapeToken} from "../src/VapeToken.sol";
+
+contract VapeTokenTest is Test {
+    VapeToken vape;
+    address payable rewards = payable(address(1234567));
+
+    function setUp() public {
+        vape = new VapeToken(rewards);
+    }
+
+    function test_takeAVapeHit_failsIfPaused() public {
+        vape.pause();
+        vm.expectRevert("Contract is paused.");
+        vape.takeAVapeHit();
+    }
+
+    function test_takeAVapeHit_failsIfBelowMinInvest() public {
+        vm.expectRevert("ETH value below min invest");
+        vape.takeAVapeHit{value: 0.0066 ether}();
+    }
+
+    function test_takeAVapeHit_failsIfTimePassed() public {
+        vm.warp(block.timestamp + 86401);
+        vm.expectRevert("Time is over, pot can be claimed by winner.");
+        vape.takeAVapeHit{value: 0.0067 ether}();
+    }
+
+    function test_takeAVapeHit_works() public {
+        for (uint256 i = 1; i <= 10; i++) {
+            vm.prank(vm.addr(i));
+            vm.deal(vm.addr(i), 1 ether);
+            vape.takeAVapeHit{value: 0.0067 ether + (0.0067 ether * i)}();
+        }
+        emit log_named_uint("balance", address(vape).balance);
+        emit log_named_uint("potValueETH", vape.potValueETH());
+        emit log_named_uint("totalDividendsValueETH", vape.totalDividendsValueETH());
+        emit log_named_uint("collectedFee", vape.collectedFee());
+        emit log_named_uint("minInvest", vape.minInvest());
+        emit log_named_uint("vapeTokenPrice", vape.vapeTokenPrice());
+        emit log_named_uint("lastPurchasedTime", vape.lastPurchasedTime());
+        emit log_named_address("lastPurchasedAddress", vape.lastPurchasedAddress());
+        emit log_named_uint("vapeBalance", vape.balanceOf(address(this)));
+    }
+}
